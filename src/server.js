91c05a8d489e270aafe4d9c067553945189b80b8
nicolasandradedesarrollosit <nicolasandradedesarrollos@gmail.com>
2025-09-26 13:31:30 -0300

    arreglo de rutas, health, node version

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 import app from './app.js';
 
-const PORT = Number(process.env.PORT || 4000);
+const PORT = Number(process.env.PORT || '0.0.0.0');
 app.listen(PORT, () => console.log(`Servidor siendo escuchado en el puerto ${PORT}`));
