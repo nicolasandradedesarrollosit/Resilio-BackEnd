5ae14c06a3abd49e58e129c94f466c2d7962c371
nicolasandradedesarrollosit <nicolasandradedesarrollos@gmail.com>
2025-10-16 19:07:12 -0300

    arreglos multer

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import userRoute from './client/route/userRoute.js';
